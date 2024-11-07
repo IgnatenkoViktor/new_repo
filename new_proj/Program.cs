e220379e1a3f091aade3e3ec0f9411c7e403f17a
IgnatenkoViktor <viktorignatenko11@gmail.com>
2024-11-07 09:18:55 +0200

    Додано зміни

---
+++ new_proj/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace qbit_loops_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Trim().Split();
+            double N = double.Parse(arr[0]);
+            double P = double.Parse(arr[1]);
+            double M = double.Parse(arr[2]);
+
+            int Y = 0;
+            double currentN = N;
+            double percentage = P / 100;
+
+            while (currentN < M)
+            {
+                currentN += currentN * percentage;
+                currentN = Math.Floor(currentN * 100) / 100;
+                Y++;
+            }
+
+            Console.WriteLine($"{Y}");
+            Console.ReadLine();
+        }
+    }
+}
